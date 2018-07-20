9c92658e67f0a85bbddc8c61eaa1b3569e14c0c1
Barna Magyarkuti <barna.magyarkuti@emarsys.com>
2018-07-20 14:44:07 +0200

    extract tokens

--- src/lib/aggregator/aggregator_embedded_actions.js
+++ src/lib/aggregator/aggregator_embedded_actions.js
@@ -1,90 +1,7 @@
 'use strict';
-const { createToken, Lexer, Parser, tokenMatcher } = require('chevrotain');
+const { Lexer, Parser} = require('chevrotain');
+const { tokens, allTokens } = require('./tokens');
 
-const UnaryOperator = createToken({
-  name: 'UnaryOperator',
-  pattern: Lexer.NA
-});
-
-const LastOperator = createToken({
-  name: 'LastOperator',
-  pattern: /LAST/,
-  categories: UnaryOperator
-});
-
-const AdditionOperator = createToken({
-  name: 'AdditionOperator',
-  pattern: Lexer.NA
-});
-
-const Plus = createToken({
-  name: 'Plus',
-  pattern: /\+/,
-  categories: AdditionOperator
-});
-const Minus = createToken({
-  name: 'Minus',
-  pattern: /-/,
-  categories: AdditionOperator
-});
-
-const MultiplicationOperator = createToken({
-  name: 'MultiplicationOperator',
-  pattern: Lexer.NA
-});
-const Multi = createToken({
-  name: 'Multi',
-  pattern: /\*/,
-  categories: MultiplicationOperator
-});
-const Div = createToken({
-  name: 'Div',
-  pattern: /\//,
-  categories: MultiplicationOperator
-});
-
-const LParen = createToken({ name: 'LParen', pattern: /\(/ });
-const RParen = createToken({ name: 'RParen', pattern: /\)/ });
-const NumberLiteral = createToken({
-  name: 'NumberLiteral',
-  pattern: /[1-9]\d*/
-});
-
-
-const PowerFunc = createToken({ name: 'PowerFunc', pattern: /power/ });
-const Comma = createToken({ name: 'Comma', pattern: /,/ });
-
-// marking WhiteSpace as 'SKIPPED' makes the lexer skip it.
-const WhiteSpace = createToken({
-  name: 'WhiteSpace',
-  pattern: /\s+/,
-  group: Lexer.SKIPPED
-});
-
-const StringLiteral = createToken({
-  name: 'StringLiteral',
-  pattern: /[\w\.]+/
-});
-
-
-const allTokens = [
-  WhiteSpace,
-  LastOperator,
-  Plus,
-  Minus,
-  Multi,
-  Div,
-  LParen,
-  RParen,
-  NumberLiteral,
-  AdditionOperator,
-  UnaryOperator,
-  MultiplicationOperator,
-  PowerFunc,
-  Comma,
-  StringLiteral
-];
-const CalculatorLexer = new Lexer(allTokens);
 
 class Calculator extends Parser {
   constructor(input, period) {
@@ -97,8 +14,8 @@ class Calculator extends Parser {
     });
 
     $.RULE('unaryExpression', () => {
-      $.CONSUME(LastOperator);
-      const key = $.CONSUME(StringLiteral).image;
+      $.CONSUME(tokens.LastOperator);
+      const key = $.CONSUME(tokens.StringLiteral).image;
       return this._period.last(key);
     });
     this.performSelfAnalysis();
@@ -106,6 +23,7 @@ class Calculator extends Parser {
 }
 
 module.exports = period => text => {
+  const CalculatorLexer = new Lexer(allTokens);
   const parser = new Calculator([], period);
   const lexResult = CalculatorLexer.tokenize(text);
   parser.input = lexResult.tokens;
