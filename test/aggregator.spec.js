7f8d11d37577dc0934232198a312543a59ba1c05
Barna Magyarkuti <barna.magyarkuti@emarsys.com>
2018-07-24 10:24:06 +0200

    refactor tests into describes

--- test/aggregator.spec.js
+++ test/aggregator.spec.js
@@ -9,77 +9,110 @@ describe('The Aggregator Grammar', () => {
 
   const aggregate = require('../src/aggregator')(period);
 
-  it('has a last operator', () => {
-    expect(aggregate('LAST campaigns.email.open').value).to.eql(4);
+  describe('Binary Operands', () => {
+    it('has a last operator', () => {
+      expect(aggregate('LAST campaigns.email.open').value).to.eql(4);
+    });
+
+    it('has a sum operator', () => {
+      expect(aggregate('SUM campaigns.email.open').value).to.eql(7);
+    });
+
+    it('has an average operator', () => {
+      expect(aggregate('AVERAGE campaigns.email.open').value).to.eql(3.5);
+    });
+
+    it('has a rule for number literals', () => {
+      expect(aggregate('1').value).to.eql(1);
+    });
   });
 
-  it('has a sum operator', () => {
-    expect(aggregate('SUM campaigns.email.open').value).to.eql(7);
+  describe('Binary Operators', () => {
+    describe('Plus Operator', () => {
+      it('exists', () => {
+        expect(aggregate('SUM campaigns.email.open + LAST campaigns.email.open').value).to.eql((3 + 4) + 4);
+      });
+
+      it('can take any binary operand', () => {
+        expect(aggregate('SUM campaigns.email.open + 1').value).to.eql((3 + 4) + 1);
+      });
+
+      it('chains', () => {
+        expect(aggregate('1 + 1 + 1').value).to.eql(3);
+      });
+    });
+
+    describe('Minus Operator', () => {
+      it('exists', () => {
+        expect(aggregate('SUM campaigns.email.open - LAST campaigns.email.open').value).to.eql(3 + 4 - 4);
+      });
+
+
+      it('can take any binary operand', () => {
+        expect(aggregate('LAST campaigns.email.open + 1 - LAST campaigns.email.open').value).to.eql(1);
+      });
+
+      it('chains', () => {
+        expect(aggregate('1 - 1 + 1 - 1').value).to.eql(0);
+      });
+    });
+
+    describe('Multiplication Operator', () => {
+      it('exists', () => {
+        expect(aggregate('2 * 3').value).to.eql(6);
+      });
+
+      it('chains', () => {
+        expect(aggregate('2 * 2 * 2 * 2').value).to.eql(2 ** 4);
+      });
+
+      it('has a higher precedence than the plus operator', () => {
+        expect(aggregate('2 + 3 * 3').value).to.eql(11);
+      });
+
+      it('has a higher precedence than the minus operator', () => {
+        expect(aggregate('10 - 3 * 2').value).to.eql(4);
+      });
+    });
+
+    describe('Division Operator', () => {
+      it('exists', () => {
+        expect(aggregate('2 / 2').value).to.eql(1);
+      });
+
+      it('chains', () => {
+        expect(aggregate('20 / 2 / 5').value).to.eql(2);
+      });
+
+      it('doesnt matter how its precedence relates to the multiplication operator', () => {
+        expect(aggregate('5 * 6 / 3').value).to.eql(10);
+      });
+
+      it('has a higher precedence than the plus operator', () => {
+        expect(aggregate('2 + 3 / 3').value).to.eql(3);
+      });
+
+      it('has a higher precedence than the minus operator', () => {
+        expect(aggregate('9 - 3 / 3').value).to.eql(8);
+      });
+    })
   });
 
-  it('has an average operator', () => {
-    expect(aggregate('AVERAGE campaigns.email.open').value).to.eql(3.5);
+
+  describe('parentheses', () => {
+    it('overwrites operator precedence', () => {
+      expect(aggregate('(10 + 5) * 3').value).to.eql(45);
+    });
+
+    it('can be placed around any number expression', () => {
+      expect(aggregate('(((123)))').value).to.eql(123);
+    });
   });
 
-  it('has an addition operator', () => {
-    expect(aggregate('SUM campaigns.email.open + LAST campaigns.email.open').value).to.eql((3 + 4) + 4);
-  });
 
-  it('has an addition operator that works with number literals', () => {
-    expect(aggregate('SUM campaigns.email.open + 1').value).to.eql((3 + 4) + 1);
-  });
-
-  it('passes through a number literal', () => {
-    expect(aggregate('1').value).to.eql(1);
-  });
-
-  it('has a minus operator', () => {
-    expect(aggregate('SUM campaigns.email.open - LAST campaigns.email.open').value).to.eql(3 + 4 - 4);
-  });
-
-  it('can chain addition operators', () => {
-    expect(aggregate('1 + 1 + 1').value).to.eql(3);
-  });
-
-  it('can chain binary and unary operators', () => {
-    expect(aggregate('LAST campaigns.email.open + 1 - LAST campaigns.email.open').value).to.eql(1);
-  });
-
-  it('can chain minus, plus operators', () => {
-    expect(aggregate('1 - 1 + 1 - 1').value).to.eql(0);
-  });
-
-  it('has a multiplication operator', () => {
-    expect(aggregate('2 * 3').value).to.eql(6);
-  });
-
-  it('applies the right precedence to the multiplication operator', () => {
-    expect(aggregate('2 + 3 * 3').value).to.eql(11);
-  });
-
-  it('chains multiplication operators correctly', () => {
-    expect(aggregate('2 * 2 * 2 * 2').value).to.eql(2 ** 4);
-  });
-
-  it('has a division operator', () => {
-    expect(aggregate('2 / 2').value).to.eql(1);
-  });
-
-  it('chains division operators correctly', () => {
-    expect(aggregate('20 / 2 / 5').value).to.eql(2);
-  });
-
-  it('applies the right precedence to the division operator', () => {
-    expect(aggregate('10 + 5 * 6 / 3').value).to.eql(20);
-  });
-
-  it('works correctly for expressions with parentheses', () => {
-    expect(aggregate('(10 + 5) * 3').value).to.eql(45);
-  });
-
-  describe('when there is a parsing error', function() {
+  describe('when there is a parsing error', () => {
     it('throws an exception', () => {
       expect(() => aggregate('INVALID_TOKEN')).to.throw();
-    })
+    });
   })
 });
