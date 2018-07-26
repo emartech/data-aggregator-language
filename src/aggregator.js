9a364783ae25b1fa9b14d5ad30014a5c68f0223e
Barna Magyarkuti <barna.magyarkuti@emarsys.com>
2018-07-26 17:06:04 +0200

    refactor(aggregator): break up into shorter functions
    REPORTING-OPS

--- src/aggregator.js
+++ src/aggregator.js
@@ -7,22 +7,31 @@ const { tokens } = require('./lib/aggregator/tokens');
 const interpreterFactory = require('./lib/aggregator/interpreter');
 const Period = require('../src/lib/period/period');
 
-const CalculatorLexer = new Lexer(tokens);
-const parser = new AggregatorParser([]);
-const AggregatorInterpreter = interpreterFactory(parser);
 
-module.exports = (input) => (text) => {
-  const interpreter = new AggregatorInterpreter(Period.create(input));
-  const lexResult = CalculatorLexer.tokenize(text);
-
+const evaluate = (lexer, parser, interpreter) => (text) => {
+  const lexResult = lexer.tokenize(text);
   parser.input = lexResult.tokens;
   const cst = parser.additionExpression();
-
   const value = interpreter.visit(cst);
 
-  if (parser.errors.length > 0) {
-    throw new Error([`Error parsing "${text}"\n`] + parser.errors);
-  }
+  assertParsingSuccessful(parser, text);
 
   return value;
 };
+
+let assertParsingSuccessful = function(parser, text) {
+  if(parser.errors.length > 0) {
+    throw new Error([`Error parsing "${text}"\n`] + parser.errors);
+  }
+};
+
+(() => {
+  const lexer = new Lexer(tokens);
+  const parser = new AggregatorParser([]);
+  const AggregatorInterpreter = interpreterFactory(parser);
+
+  module.exports = (input) => (text) => {
+    const interpreter = new AggregatorInterpreter(Period.create(input));
+    return evaluate(lexer, parser, interpreter)(text);
+  };
+})();
